e33bde04ff01ecb1254b94ed4a8c7c4110ce65b3
BheeshmasenaReddy <96126805+BheeshmasenaReddy@users.noreply.github.com>
2023-01-19 09:10:47 +0530

    Add files via upload

---
+++ ybus_function.py
@@ -0,0 +1,78 @@
+inp=input('Enter line data in format of [[from_bus,to_bus,resistance,reactance]] :')
+s1='[[1,2,0,0.2],[2,3,0,0.25],[1,3,0,0.25]]'
+s2='[[1,2,0,0.1],[3,4,0,0.1],[1,4,0,0.2],[1,3,0,0.5],[2,3,0,0.2],[1,0,0,0.2]]'
+def adm_mat(p):
+    linedata=eval(s2)
+
+    #Reading linedata
+    fb=[i[0] for i in linedata]
+    tb=[i[1] for i in linedata]
+    r=[i[2] for i in linedata]
+    x=[i[3] for i in linedata]
+
+    z=[complex(r[k],x[k]) for k in range(len(r))] #Calculating primitive impedence
+    y=[(1/j) for j in z] #Calculating admittance
+
+    nbus=max(max(fb),max(tb))
+    nbranch=len(fb)
+
+    ybus=[]
+
+    #creating a ybus as null matrix
+    for i in range(nbus):
+        ybus.append([0]*nbus)
+
+    #formation of mutual admittance
+    for i in range(nbranch):
+        if fb[i]!=0 or tb[i]!=0:
+            ybus[fb[i]-1][tb[i]-1]-=(y[i])
+            ybus[tb[i]-1][fb[i]-1]=ybus[fb[i]-1][tb[i]-1]
+
+    #formation of self admittance
+    for m in range(nbranch):
+        for n in range(nbranch):
+            if (fb[n]-1)==m:
+                ybus[m][m]+=y[n]
+            elif (tb[n]-1)==m:
+                ybus[m][m]+=y[n]
+
+    #function for printing ybus and linedata
+    def mat_print(lis):
+        x=len(lis)
+        y=len(lis[0])
+        for i in range(x+1):
+            for j in range(y+1):
+                if i==0 and j==0:
+                    print("   ",end=" ")
+                elif i==0: 
+                    print(j,end="   ")
+                elif j==0:
+                    print(i,end='  ')
+                else:
+                    print(lis[i-1][j-1],end='  ')
+            print("\n")
+            
+    #making input data
+    inp_data=[]
+    data=[fb,tb,z]
+    for i in range(len(linedata)):
+        inp_data.append([0]*len(data))
+        
+    for i in range(len(linedata)):
+        for j in range(len(data)):
+            inp_data[i][j]=data[j][i]
+
+    #printing output
+    print('Adjacency matrix [A] of the given power systems network:')
+    mat_print(inp_data)
+    print('\nNo.of buses:',nbus)
+    print('\nNo. of transmission lines:',nbranch)
+    for i in range(len(z)):
+        print(f'\nPrimitive impedance of line {i+1}:',z[i],'ohms')
+    print('\nBus admittance matrix:\nYBUS=')
+    mat_print(ybus)
+
+#driver code
+if __name__=="__main__":
+    adm_mat(s2)
+#    adm_mat(inp)
